e69050617d3a45d1378707b24e707ff0827f3381
BaselQq <73541125+BaselQq@users.noreply.github.com>
2024-04-15 14:54:49 +0200

    add task

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,19 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+// Step 1: Create an array of numbers. Use the '.map' function to double all the numbers in the array.
+const numbers: number[] = [1,2,3,4,5,6,7,8];
+const doubledNumbers: number[] = numbers.map((number: number) => number * 2);
+console.log(doubledNumbers);
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+// Step 2: Create an array of words. Use the '.filter' function to select only the words that are longer than 5 letters.
+const words: string[] = ["hello", "beautiful", "world"];
+const filteredWords: string[] = words.filter((word: string) => word.length > 5)
+console.log(filteredWords);
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+// Step 3: Create an array of numbers. Use the '.reduce' function to calculate the sum of all the numbers in the array.
+const numbers1: number[] = [1,2,3,4,5,6,7,8];
+const reducedNumbers: number = numbers1.reduce((accumulator: number, currentValue: number) => accumulator + currentValue);
+console.log(reducedNumbers);
+
+// Step 4: Create an array of numbers. Use the '.some' function to check if at least one number is greater than 10.
+const numbers2: number[] = [1,2,3,4,5,6,7,8,20];
+const checkedNumbers: boolean = numbers2.some((number: number) => number > 10);
+console.log(checkedNumbers)
